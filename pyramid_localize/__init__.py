4aba6c65a7188816e58c068272e28e0be264c6dd
Grzegorz Śliwiński <fizyk@fizyk.net.pl>
2013-09-16 22:50:11 +0200

    Release v0.0.5

--- pyramid_localize/__init__.py
+++ pyramid_localize/__init__.py
@@ -13,7 +13,7 @@ from pyramid_localize.request import database_locales
 from pyramid_localize.request import locales
 from pyramid_localize.request import locale_id
 
-__version__ = '0.0.4'
+__version__ = '0.0.5'
 
 
 def includeme(configurator):
