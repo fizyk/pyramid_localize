6bf0a05f5199f9f4fdf9460762e21a151d259976
Grzegorz Śliwiński <fizyk@fizyk.net.pl>
2013-09-18 21:31:51 +0200

    Prerelease 0.1a2

--- pyramid_localize/__init__.py
+++ pyramid_localize/__init__.py
@@ -18,7 +18,7 @@ from pyramid_localize.request import database_locales
 from pyramid_localize.request import locales
 from pyramid_localize.request import locale_id
 
-__version__ = '0.1a1'
+__version__ = '0.1a2'
 
 
 def includeme(configurator):
