7a11124785f2f402809f4e9d9540023671912426
Grzegorz Śliwiński <fizyk@fizyk.net.pl>
2014-05-03 00:55:39 +0200

    python 3.4 compatibility

--- setup.py
+++ setup.py
@@ -55,6 +55,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
     packages=find_packages(),
